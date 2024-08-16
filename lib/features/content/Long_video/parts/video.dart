1b1a921a4d7e8b4d625d165dbeafcb16961a2177
Kabya Chalise <kabyachalise123@gmail.com>
2024-08-16 06:49:56 +0545

    video player ui added with package

--- lib/features/content/Long_video/parts/video.dart
+++ lib/features/content/Long_video/parts/video.dart
@@ -1,14 +1,32 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 import 'package:youtube_clone/cores/colors.dart';
 import 'package:youtube_clone/cores/widgets/flat_button.dart';
 import 'package:youtube_clone/features/content/Long_video/widgets/video_externel_buttons.dart';
 
-class Video extends StatelessWidget {
+class Video extends StatefulWidget {
   const Video({Key? key}) : super(key: key);
 
   @override
+  State<Video> createState() => _VideoState();
+}
+
+class _VideoState extends State<Video> {
+  bool isShowIcon = false;
+  bool isPlaying = false;
+  VideoPlayerController? _controller;
+  @override
+  initState() {
+    super.initState();
+    _controller = VideoPlayerController.networkUrl(Uri.parse(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'))
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,20 +36,54 @@ class Video extends StatelessWidget {
         bottom: PreferredSize(
           preferredSize: const Size.fromHeight(176),
           child: GestureDetector(
-            onTap: () {},
+
+            onTap: isShowIcon ? (){
+              setState(() {
+                isShowIcon = false;
+              });
+            } : () {
+              setState(() {
+                isShowIcon = true;
+              });
+            },
             child: Stack(
               children: [
-                Positioned(
-                  left: 182,
-                  top: 87,
+                AspectRatio(
+                  aspectRatio: _controller!.value.aspectRatio,
+                  child: VideoPlayer(_controller!)),
+                isShowIcon ? Positioned(
+                  left: 30,
+                  top: 82,
                   child: GestureDetector(
                     onTap: () {},
                     child: SizedBox(
                       height: 50,
-                      child: Container(),
+                      child: Image.asset('assets/images/go_back_final.png',color: Colors.white,),
                     ),
                   ),
-                ),
+                ) : SizedBox(),
+                isShowIcon? Positioned(
+                  right: 30,
+                  top: 82,
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: SizedBox(
+                      height: 50,
+                      child: Image.asset('assets/images/go ahead final.png',color: Colors.white,),
+                    ),
+                  ),
+                ): SizedBox(),
+                isShowIcon? Positioned(
+                  left: 170,
+                  top: 82,
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: SizedBox(
+                      height: 50,
+                      child: Image.asset('assets/images/play.png',color: Colors.white,),
+                    ),
+                  ),
+                ): SizedBox(),
                 Align(
                   alignment: Alignment.bottomCenter,
                   child: SizedBox(
@@ -47,13 +99,16 @@ class Video extends StatelessWidget {
       body: SafeArea(
         child: ListView(
           children: [
-            Text(
-              "How to learn Flutter quickly",
-              overflow: TextOverflow.ellipsis,
-              softWrap: true,
-              style: const TextStyle(
-                fontSize: 17,
-                fontWeight: FontWeight.w500,
+            Padding(
+              padding: const EdgeInsets.only(left: 12),
+              child: Text(
+                "How to learn Flutter quickly",
+                overflow: TextOverflow.ellipsis,
+                softWrap: true,
+                style: const TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
             Padding(
@@ -64,6 +119,16 @@ class Video extends StatelessWidget {
                     padding: const EdgeInsets.only(left: 8, right: 8),
                     child: Text(
                       "No view",
+                      style: const TextStyle(
+                        fontSize: 13.4,
+                        color: Color(0xff5F5F5F),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 4),
+                    child: Text(
+                      "5 minutes ago",
                       style: const TextStyle(
                         fontSize: 13.4,
                         color: Color(0xff5F5F5F),
@@ -97,6 +162,19 @@ class Video extends StatelessWidget {
                       ),
                     ),
                   ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      right: 6,
+                    ),
+                    child: Text(
+                      "1 subscriber",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  Spacer(),
                   SizedBox(
                     height: 35,
                     width: 100,
@@ -138,7 +216,7 @@ class Video extends StatelessWidget {
                               size: 15.5,
                             ),
                           ),
-                          const SizedBox(width: 5),
+                          const SizedBox(width: 15),
                           const Icon(
                             Icons.thumb_down,
                             size: 15.5,
